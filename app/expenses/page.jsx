c88ca6c245011883f92326b114dc734cd7958b7b
Shubb3006 <107855908+Shubb3006@users.noreply.github.com>
2025-09-06 14:34:18 +0530

    final

--- app/expenses/page.jsx
+++ app/expenses/page.jsx
@@ -87,7 +87,7 @@ const page = () => {
             <div className="text-center bg-green-100 dark:bg-green-500 text-green-800 dark:text-green-100 font-semibold py-3 rounded-md shadow-md mt-6">
               Total Expenses: ₹{totalExpense}
             </div>
-            <div className="text-center bg-green-100 dark:bg-green-500 text-green-800 dark:text-green-100 font-semibold py-3 rounded-md shadow-md mt-6">
+            <div className="text-center bg-green-100 dark:bg-green-500 text-green-800 dark:text-green-100 font-semibold py-3 rounded-md shadow-md mt-2">
               Average Monthly Expenses: ₹{totalExpense/month_num}
             </div>
           </>
